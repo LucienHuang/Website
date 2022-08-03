268ecf70b0d94999bc954c810cccb62585dea887
Lucien Huang <superstarhyc@qq.com>
2022-08-03 13:11:39 +0800

    Update index.ts

--- src/api/mint/index.ts
+++ src/api/mint/index.ts
@@ -8,23 +8,23 @@ import { serveRequest } from '../axios'
 const demoNFTItem: NFTItem = {
   information: {
     images: [
-      'https://i.imgur.com/CRvY7vc.png',
-      'https://i.imgur.com/IBqASlD.png',
-      'https://i.imgur.com/nmDWQ0m.png'
+      'https://ambrus.s3.amazonaws.com/1659502593244_0.08_banner_480w_480w.jpg',
+      'https://ambrus.s3.amazonaws.com/1659502593218_0.20_banner_960w_960w.jpg',
+      'https://ambrus.s3.amazonaws.com/1659502593241_0.88_banner_1440w_1440w.jpg'
     ],
     type: 'E4C Rangers NFT Series 1',
     name: 'Rin: Heir of Musashi',
     content:
-      "Pearce's fixed-wing X6 provides a surprising amount of comfortable living space aboard a compact and highly agile fighter patrol ship. A supremely capable combat vessel with an extended atmoglass canopy provides a conspicuous piloting advantage as well as a way"
+      "She was born in 2063 as a human and re-born in 2079 as a combat mecha. Because her family celebrated the existence of samurai ancestors, she carries the razor-sharp katana sword and scaled samurai armor. Rin has become an infamous spectral force among criminal forces throughout the central areas of Japan’s Honshu island."
   },
   disclaimer: {
     images: [
-      'https://imgur.com/V0xOBYB.png',
-      'https://imgur.com/V0xOBYB.png',
-      'https://imgur.com/V0xOBYB.png'
+      'https://ambrus.s3.amazonaws.com/1659503426906_0.55_1.jpg',
+      'https://ambrus.s3.amazonaws.com/1659503426929_0.47_2.jpg',
+      'https://ambrus.s3.amazonaws.com/1659503426933_0.29_3.jpg'
     ],
     content:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla id vestibulum diam. Sed aliquet dolor leo, eu facilisis magna porta vel. Etiam vulputate augue ac odio suscipit interdum. Donec nec auctor lectus, eu molestie odio. Nullam euismod, felis eu venenatis tincidunt, arcu enim dictum est, quis viverra nisl est quis est. Nulla facilisi. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Mauris tincidunt est nec tortor pellentesque finibus. Suspendisse lacinia eu metus id lacinia. Nullam elementum nisl dui, ut iaculis est tristique eget. Suspendisse odio magna, gravida ultricies molestie in, blandit vel dui. Nam convallis ante lacus, a interdum orci bibendum ut. Sed placerat augue vehicula diam bibendum commodo. Sed a porta orci.'
+      ''
   },
   editions: [],
   // editions: [
@@ -42,21 +42,21 @@ const demoNFTItem: NFTItem = {
     {
       title: 'NFT Sale',
       content:
-        'Ultimate Edition (15) - English auction & Open-bid 2nd price\n\nGold Edition (450) - Fixed price'
+        'Ultimate Edition(15) - English auction & Open-bid 2nd price\nGold Edition(450) - Fixed price at 0.6 ETH'
     },
     {
       title: 'NFT Utilities',
       content:
-        'E4C Rangers NFTs’ utilities are mostly the same for different editions. But if you are an Ultimate Edition owner, you’ll earn a lot more benefits in our array of games and more! [Learn more](https://www.ambrus.studio)\n\nWhat you get with Gold Edition:\n\n* 5% revenue share of corresponding hero/skin\n* 10% off in-game discounts\n* Gold loot airdrops\n* In-game asset airdrops\n* 1 free gift to friends'
+        'What you get with Gold Edition:\n* 5% in-game IP dividend ownership of the corresponding champions+skins(shared among holders).Airdrop of the corresponding champion in games.\n* Gold Loot Box containing airdrops of in-game champions and skins.\n* 10%  discount on in-game purchases.\n* Complementary gifts to 1 friend.'
     },
     {
-      title: 'NFT Staking',
+      title: 'NFT Staking System',
       content:
-        'For Gold Edition owners, we offer you a chance to upgrade your NFT to Gold+ and earn more benefits by simply staking your E4C Rangers NFT for 3 months. Each NFT can only upgrade once.\n\nAfter upgraded to Gold+, you’ll get:\n\n* 8% revenue share of corresponding hero/skin\n* 15% off in-game discounts\n* Gold+ loot airdrops\n* More In-game asset airdrops\n* 3 free gift to friends'
+        'For Gold Edition holders, we offer you a chance to upgrade your NFT to Gold+ and earn more benefits by\n* Staking E4C Rangers NFT for at least 90 days.\n* Completing specific community missions and claiming the badge.'
     },
     {
-      title: 'Additional Information',
-      content: 'Descriptions'
+      title: 'Utilities of the Gold+ Edition',
+      content: '5% in-game IP dividend ownership shared among holders and 3% additional IP dividend ownership of the corresponding character (shared among holders of Ultimate and Gold+ Edition).\n* E4C token airdrop.\n* Airdrop of the corresponding ranger in games.\n* Gold+ Loot Box airdrop of rangers and skins.\n* 15% discount on in-game purchases.\n* Complementary gifts to 3 friends.\n\nIn addition, holders who stake will also be granted 1 NFT airdrop from the next release.'
     }
   ]
 }
